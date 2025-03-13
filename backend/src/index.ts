9be1066e7b6f5014decad0fdb2122583bc1d8ce3
tapas@kofukuhcs.com <tapas@kofukuhcs.com>
2025-03-13 05:05:17 +0000

    fixing the node js with typescript issue

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,12 @@
 import express from "express";
-import { Pool } from "pg";
-import dotenv from "dotenv";
-import cors from "cors";
-
-dotenv.config();
 
 const app = express();
-app.use(express.json());
-app.use(cors());
+const PORT = process.env.PORT || 4000;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: 5432,
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript with Express and ESM!");
 });
 
-app.get("/", async (req, res) => {
-  // const result = await pool.query("SELECT NOW()");
-  // res.json(result.rows);
-  // res.json("hello world")
-  
+app.listen(PORT, () => {
+  console.log(`⚡ Server is running at http://localhost:${PORT}`);
 });
-
-app.listen(4000, () => console.log("Server running on port 4000"));
